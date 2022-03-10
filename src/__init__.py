d5997b49ebd021910ff249e6d41239b4cfcbfe87
muradddd <murad.rustemzade@gmail.com>
2022-03-10 16:40:26 +0400

    initial commit

---
+++ src/__init__.py
@@ -0,0 +1,60 @@
+from flask.json import jsonify
+from src.constants.http_status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
+from flask import Flask, config, redirect
+import os
+from src.auth import auth
+from src.todos import todos
+from src.database import db, Todo
+from flask_jwt_extended import JWTManager
+from flasgger import Swagger, swag_from
+from src.config.swagger import template, swagger_config
+
+
+def create_app(test_config=None):
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY'),
+
+
+            SWAGGER={
+                'title': "Todos API",
+                'uiversion': 3
+            }
+        )
+    else:
+        app.config.from_mapping(test_config)
+
+    db.app = app
+    db.init_app(app)
+
+    JWTManager(app)
+    app.register_blueprint(auth)
+    app.register_blueprint(todos)
+
+    Swagger(app, config=swagger_config, template=template)
+
+    @app.get('/<short_url>')
+    @swag_from('./docs/short_url.yaml')
+    def redirect_to_url(short_url):
+        todo = Todo.query.filter_by(short_url=short_url).first_or_404()
+
+        if todo:
+            todo.visits = todo.visits+1
+            db.session.commit()
+            return redirect(todo.url)
+
+    @app.errorhandler(HTTP_404_NOT_FOUND)
+    def handle_404(e):
+        return jsonify({'error': 'Not found'}), HTTP_404_NOT_FOUND
+
+    @app.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
+    def handle_500(e):
+        return jsonify({'error': 'Something went wrong, we are working on it'}), HTTP_500_INTERNAL_SERVER_ERROR
+
+    return app
---
+++ src/database.py
@@ -0,0 +1,46 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import string
+import random
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.Text(), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+    todos = db.relationship('Todo', backref="user")
+
+    def __repr__(self) -> str:
+        return 'User>>> {self.username}'
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=True)
+    is_complete = db.Column(db.Boolean, default=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+
+    def generate_short_characters(self):
+        characters = string.digits+string.ascii_letters
+        picked_chars = ''.join(random.choices(characters, k=3))
+
+        link = self.query.filter_by(short_url=picked_chars).first()
+
+        if link:
+            self.generate_short_characters()
+        else:
+            return picked_chars
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.short_url = self.generate_short_characters()
+
+    def __repr__(self) -> str:
+        return 'Todo>>> {self.url}'
