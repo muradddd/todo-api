fe5489cffe86cbdcffe5790aab0f4dfd3b4136cc
muradddd <murad.rustemzade@gmail.com>
2022-03-11 04:15:19 +0400

    todo model modified

--- src/database.py
+++ src/database.py
@@ -25,22 +25,10 @@ class Todo(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, onupdate=datetime.now())
-
-    def generate_short_characters(self):
-        characters = string.digits+string.ascii_letters
-        picked_chars = ''.join(random.choices(characters, k=3))
-
-        link = self.query.filter_by(short_url=picked_chars).first()
-
-        if link:
-            self.generate_short_characters()
-        else:
-            return picked_chars
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        self.short_url = self.generate_short_characters()
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
     def __repr__(self) -> str:
         return 'Todo>>> {self.url}'
