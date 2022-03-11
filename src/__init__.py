f6ed209cbb2212ea89651137f9d08d804a9f15df
muradddd <murad.rustemzade@gmail.com>
2022-03-11 04:14:43 +0400

    cors changed

--- src/__init__.py
+++ src/__init__.py
@@ -8,12 +8,13 @@ from src.database import db, Todo
 from flask_jwt_extended import JWTManager
 from flasgger import Swagger, swag_from
 from src.config.swagger import template, swagger_config
+from flask_cors import CORS
 
 
 def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
-
+    CORS(app)
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
